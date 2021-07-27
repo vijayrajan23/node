0318571a6ed1c7aa0a997570adbe6c2308508898
vijayrajan23 <vijayrajan23@gmail.com>
2021-07-28 00:11:23 +0530

    stage

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=> {
-	res.send('Node Test page')
+	res.send('Welcome our school')
 })
 
 app.listen(8080, () => {
